a61e66aafade024355c54a23475d330f9f927eeb
3lfive <denystry@gmail.com>
2023-04-06 18:09:55 -0400

    feat: add some header

--- Client/src/Components/Header/header.component.jsx
+++ Client/src/Components/Header/header.component.jsx
@@ -1,11 +1,34 @@
 import React from "react";
+import NavigationMain from "./NavigationMain/NavigationMain.component";
 
+const navList =[
+    {
+        title: "Головна",
+        path: "/",
+    },
+    {
+        title: "Партнерам",
+        path: "/partners",
+    },
 
-
+    {
+        title: "Наші магазини",
+        path: "/shops",
+    },
+    {
+        title: "Про нас",
+        path: "/aboutus",
+    },
+    {
+        title: "Акції",
+        path: "/disconts",
+    }
+]
 const Header = (props) => {
 
     return(
         <>
+        <NavigationMain navList={navList}/>
         <h1>Header block</h1>
     </>);
     }
