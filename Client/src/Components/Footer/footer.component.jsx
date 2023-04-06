829a23204621b89fb3dce3f490ed82385149472e
3lfive <denystry@gmail.com>
2023-04-06 15:39:42 -0400

    init: start project add some routes,pages,few comp

---
+++ Client/src/Components/Footer/footer.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const Footer = (props) => {
+
+    return(
+        <>
+        <h1>Footer block</h1>
+    </>);
+    }
+export default Footer;
---
+++ Client/src/Pages/Home/home.page.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+function Home(props) {
+
+    return (
+        <main>
+                ddd
+        </main>
+    );
+
+}
+
+
+
+export default Home;
