989b2b435cbd3897ec14202973ceb1554fdf3267
3lfive <denystry@gmail.com>
2023-04-10 09:56:53 -0400

    fix: fix search input

--- Client/src/Components/Header/MidleMenu/SearchBar/searchbar.component.jsx
+++ Client/src/Components/Header/MidleMenu/SearchBar/searchbar.component.jsx
@@ -1,41 +1,36 @@
-import TextField from "@mui/material/TextField";
-import IconButton from "@mui/material/IconButton";
-import SearchIcon from "@mui/icons-material/Search";
-import Box from '@mui/material/Box';
-import Input from '@mui/material/Input';
-
 import React from 'react';
 import { Link } from "react-router-dom";
+
+import Paper from '@mui/material/Paper';
+import IconButton from "@mui/material/IconButton";
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from "@mui/icons-material/Search";
 
 
 const SearchBar =()=>{
   
     return(
-         <Box sx={{height:"52px",width:"486px", display:"flex",justifyContent: 'flex-start'}}>
-         
-            <TextField   sx={{width:"440px", '& .MuiOutlinedInput-root': {
-                '& fieldset': {
-                  borderRadius: `0 0 0 0`,height:"52.6px","border-color":"A0A9AF"
-                }, '&:hover fieldset': {"border-color":"#A0A9AF"},'&.Mui-focused fieldset': {
-                    borderColor: '#007042', height:"50.6px"
-                  }
-              }
-}} id="search-bar" 
-                variant="outlined"
-                placeholder="Пошук меблів"
-                size="medium">
-                </TextField>
-
-
-            <IconButton type="submit" aria-label="search" sx={{height:"49.5px", border: 0,backgroundColor: "#A0A9AF", borderRadius: 0,
-            '&:hover, active':{
+        <Paper
+        component="form" variant="outlined"
+        sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400,borderRadius:0,height:40 }}
+      >
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Пошук меблів"
+          inputProps={{ 'aria-label': 'search google maps' }}
+        />
+             <IconButton type="submit" aria-label="search" sx={{ mr:"-5px",p:"11px",border: "0",backgroundColor: "#A0A9AF", borderRadius: 0,
+                '&:hover, active':{
                 backgroundColor: '#007042'}
-        }}>
+                }}>
                 <SearchIcon sx={{ fill: "white"}}/>        
             </IconButton>
-        </Box>  
+      </Paper>
+
+         
               
     )
     }
     
     export default SearchBar;
+   
