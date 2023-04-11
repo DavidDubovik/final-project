93c1ad2dd9f3c1ecd609b844ed599b1c12510fa3
Denys <denystry@gmail.com>
2023-04-11 19:33:22 +0300

    Revert "add main slider"

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import "./index.css"
 import myTheme from './Theme/Theme';
@@ -7,16 +8,10 @@ import { ThemeProvider } from '@mui/material/styles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-
   <ThemeProvider theme={myTheme}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </ThemeProvider>
-
 );
 
