62dc4379c70ce33371ca722e61150af6a1a1048a
Friends0810 <DavidDubovik19@gmail.com>
2023-04-12 11:57:01 +0300

    popular-products fix

--- Client/src/Components/PopularProducts/index.js
+++ Client/src/Components/PopularProducts/index.js
@@ -4,36 +4,7 @@ import CardActionArea from '@mui/material/CardActionArea';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-
-
-const useStyles = {
-    root: {
-        margin: '55px 11.5px 77px',
-        maxWidth: 282,
-        width: 282,
-        position: 'relative'
-    },
-    new: {
-        color: '#FFFFFF',
-        transform: 'translate(10px, 20px) rotate(45deg)',
-        background: '#E4A11E',
-        zIndex: 2,
-        padding: '5px 20px',
-        position: 'absolute',
-        right: -14,
-        top: -24
-    },
-    imgProduct: {
-        width: 280,
-        height: 280
-    },
-    img: {
-        width: '100%',
-        objectFit: 'cover',
-    },
-
-
-};
+import useStyles from "./style";
 
 
 export default function ImgMediaCard() {
@@ -41,8 +12,8 @@ export default function ImgMediaCard() {
         products: [
             {
                 "name": "Стілець обідній Юля горіх темний/сірий",
-                "price": '1000₴',
-                "img_url": "https://wowin.ua/image/cache/catalog/easyphoto/392/Ralf%20Lucky2-702x467h.jpg",
+                "price": '1000',
+                "img_url": "https://wowin.ua/image/cache/catalog/easyphoto/326/P1/monolith_61-702x467.jpg",
                 "id": "00110010",
                 "category": "tires",
 
@@ -50,17 +21,17 @@ export default function ImgMediaCard() {
             {
                 "name": "Ліжко-диван прямий Меблі Прогрес Кельн темно-сірий темно-сірий 1520x920x890 мм",
                 "decription": "бла-бла-бла-бла",
-                "price": '12 246₴',
-                "img_url": "https://cdn.27.ua/190/08/0b/6555659_2.jpeg",
+                "price": '12 246',
+                "img_url": "https://wowin.ua/image/cache/catalog/easyphoto/492/P2/alabama_light_grey-702x467.jpg",
                 "id": "11001441",
                 "category": "tires",
 
             },
             {
-                "name": "Тумба мобільна Грейд Ален 402х670х450 мм дуб крафт золотий / графіт",
+                "name": "Лейзі лежанка для тварин",
                 "decription": "бла-бла-бла-бла",
-                "price": '1834₴',
-                "img_url": "https://cdn.27.ua/190/58/ff/6838527_1.jpeg",
+                "price": '1834',
+                "img_url": "https://wowin.ua/image/cache/catalog/easyphoto/212/P2/california_flamingo-702x467.jpg",
                 "id": "110011",
                 "category": "tires",
 
@@ -68,8 +39,8 @@ export default function ImgMediaCard() {
             {
                 "name": "Обідній стіл у стилі лофт Серія 1 1200х600 мм",
                 "decription": "бла-бла-бла-бла ",
-                "price": '2834₴',
-                "img_url": "https://cdn.27.ua/sc--media--prod/default/7d/c1/5e/7dc15ec8-c29c-4cee-99d8-839205d3b4c4.jpg",
+                "price": '2834',
+                "img_url": "https://wowin.ua/image/cache/catalog/easyphoto/334/_MG_0983-702x467h.jpg",
                 "id": "0001111",
                 "category": "tires",
                 "newProduct": true
@@ -81,24 +52,21 @@ export default function ImgMediaCard() {
 
             <Card style={useStyles.root}>
                 {newProduct ? <p style={useStyles.new}> Новинка</p> : ''}
-                <CardActionArea>
-                    <div style={useStyles.imgProduct}>
-                        {/*<img src={img_url} alt="#" style={useStyles.img}/>*/}
-                        <CardMedia
-                            component="img"
-                            alt="Contemplative Reptile"
-                            max-height="150"
-                            style={useStyles.img}
-                            image={img_url}
-                            title="Contemplative Reptile"
-                        />
-                    </div>
+                <CardActionArea style={{height: '100%'}}>
+                    <CardMedia
+                        component="img"
+                        alt="Contemplative Reptile"
+                        max-height="150"
+                        image={img_url}
+                        title="Contemplative Reptile"
+                    />
                     <CardContent>
-                        <Typography gutterBottom variant="h5" component="h5" style={{fontSize: 15}}>
+                        <Typography gutterBottom variant="h5" component="h5" style={useStyles.nameProduct}>
                             {name}
                         </Typography>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                            ціна: {price}
+                        <Typography variant="body2" color="textSecondary" component="p" style={useStyles.price}>
+                            <p style={{margin: 0, position: 'absolute', bottom: 3}}> {price} <span
+                                style={{fontSize: 18}}>₴</span></p>
                         </Typography>
                     </CardContent>
                 </CardActionArea>
@@ -108,9 +76,13 @@ export default function ImgMediaCard() {
     })
     return (
         <div>
-
-            <div style={{display: "flex", justifyContent: 'center'}}>
-
+            <div style={{
+                display: "grid",
+                justifyContent: 'center',
+                gridTemplateColumns: 'repeat(auto-fill, 300px)',
+                maxWidth:1499,
+                margin:'0 auto'
+            }}>
                 {product}
             </div>
         </div>
---
+++ Client/src/Components/PopularProducts/style.js
@@ -0,0 +1,50 @@
+const useStyles = {
+    root: {
+        margin: '55px 11.5px 77px',
+        maxWidth: 282,
+        width: 282,
+        height: '300px',
+        position: 'relative',
+
+    },
+    new: {
+        color: '#FFFFFF',
+        transform: 'translate(10px, 20px) rotate(45deg)',
+        background: '#E4A11E',
+        zIndex: 2,
+        padding: '5px 20px',
+        position: 'absolute',
+        right: -14,
+        top: -24
+    },
+    imgProduct: {
+        width: 280,
+        height: 180
+    },
+    img: {
+        width: '100%',
+        objectFit: 'cover',
+    },
+    nameProduct: {
+        fontFamily: 'Open Sans',
+        fontStyle: 'normal',
+        fontWeight: 400,
+        fontSize: 14,
+        lineHeight: '96.5%',
+        letterSpacing: '0.01em',
+        color: '#000000'
+    },
+    price: {
+        fontFamily: 'Montserrat',
+        fontStyle: 'normal',
+        fontWeight: 800,
+        fontSize: 30,
+        lineHeight: '96.5%',
+        letterSpacing: '0.02em',
+        color: '#007042',
+
+    }
+
+
+};
+export default useStyles
