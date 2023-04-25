4598b78aeac297f2bc243715799d9c1705a51a84
Denys <denystry@gmail.com>
2023-04-25 17:33:23 +0300

    Revert "One product page"

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,14 +1,10 @@
-
+import ImgMediaCard from "./Components/PopularProducts";
 import React from 'react';
-import { Routes, Route} from 'react-router-dom';
-
-import './App.css';
-
 import Layout from './Components/Layout/layout.component';
 import Home from './Pages/Home/home.page';
 import PageNotFound from './Pages/NotFoundPage/pageNotFound.page';
-import OneProduct from "./Pages/OneProduct/OneProduct.component";
-
+import { Routes, Route} from 'react-router-dom';
+import './App.css';
 function App() {
   return (
     <div className="App">
@@ -16,7 +12,6 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="*" element={<PageNotFound />} />
-          <Route path="/products/one" element={<OneProduct />} />
         </Route>
       </Routes>
     </div>
