8c9df85fe20423cf48dd93907bc27270bf61288d
3lfive <denystry@gmail.com>
2023-04-26 12:40:34 -0400

    feat: add routing to products fix some styles

--- Client/src/Components/Chairs/ChairsItem/ChairsItem.jsx
+++ Client/src/Components/Chairs/ChairsItem/ChairsItem.jsx
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
 import './ChairsItem.scss';
+import { Link } from "react-router-dom";
 
 export class ChairsItem extends Component {
 
-   info() {
-    console.log(EventTarget)
-   }
-  
+
   render() {
     return (
       <div className='chair'>
+      <Link to={`/products/`+this.props.chair.itemNo} relative="path">
         <div className='chair-img__container'>
-            <img src={"./img/" + this.props.chair.img} alt="" />
+            <img src={this.props.chair.imageUrls[0]} alt="" />
         </div>
         <p className='chair__title'>
-            {this.props.chair.title}
+            {this.props.chair.name}
         </p>
-        <p className='chair__price'>{this.props.chair.price} <span className='chair__currency'>грн</span></p>
+        </Link>
+        <p className='chair__price'>{this.props.chair.currentPrice} <span className='chair__currency'>грн</span></p>
         <button type='button' className='chair__button' onClick={() => this.info()}>У кошик <img src="./img/cart_icon.png" alt="" /></button>
       </div>
     )
