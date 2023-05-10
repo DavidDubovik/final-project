d82a03e5aa703df99d1f8fcb0169ca5a2fbf7055
Nikita  Primiskiy <nikitaprimiskiy@gmail.com>
2023-05-10 15:36:49 +0300

    add confirm page cart(2.0.0)

--- Client/src/Pages/Cart-page/CartPage.js
+++ Client/src/Pages/Cart-page/CartPage.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { useFormik } from 'formik';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
 
 import StyllePage from "./StyllePage.scss";
 
