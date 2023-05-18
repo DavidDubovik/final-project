c72665c6b3808c6a05563779c78d648cfd31264f
3lfive <denystry@gmail.com>
2023-05-18 13:01:59 -0400

    feat: popular product fisrt 4 elemts

--- Client/src/Components/PopularProducts/index.jsx
+++ Client/src/Components/PopularProducts/index.jsx
@@ -6,6 +6,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { v4 as uuidv4 } from "uuid";  
 
 import { fetchAsyncAllProducts } from "../../Redux/products.reducer";
+
 export function ImgMediaCard() {
     const [products,setProducts]=useState("")
     const dispatch = useDispatch()
@@ -13,54 +14,57 @@ export function ImgMediaCard() {
         dispatch(fetchAsyncAllProducts()).then(res=>res.payload).then((data)=>{setProducts(data)})
     })
 
-    const product = useMemo(() => products ? products.map(({ name, imageUrls, currentPrice, newProduct, discount, newPrice }) => {
-        return (
-            <Card classes={{ root: styles.rootCard }} key={uuidv4()} >
-                {discount ? <p style={useStyles.discount}>Знижка</p> : ''}
+    const product = useMemo(() => products ? products.map(({ name, imageUrls, currentPrice, newProduct, discount, newPrice },index) => {
+        if (index <=3){
+            return (
+                <Card classes={{ root: styles.rootCard }} key={uuidv4()} >
+                    {discount ? <p style={useStyles.discount}>Знижка</p> : ''}
+    
+                    <Card style={useStyles.root}>
+    
+    
+                        {newProduct ? <p style={useStyles.new}> Новинка</p> : ''}
+                        <CardActionArea style={{ height: '100%' }}>
+                            <div style={{ height: 200, width: 200, margin: ' 0 auto' }} >
+                                <CardMedia
+                                    component="img"
+                                    alt="Contemplative Reptile"
+                                    
+                                    image={imageUrls[0]}
+                                    title="Contemplative Reptile"
+                                    style={{ height: 200, width: 200, objectFit: 'contain' }}
+                                />
+                            </div>
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="h5" style={useStyles.nameProduct}>
+                                    {name}
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p" style={useStyles.price}>
+                                    {discount ?
+                                        <div style={{ bottom: 3, position: 'absolute', display: "flex", alignItems: "center" }}>
+                                            <p style={{ margin: 0 }}> {newPrice} <span
+                                                style={{ fontSize: 18 }}>грн</span></p>
+                                            <p style={{
+                                                margin: '0 0 0 13px',
+                                                color: '#A0A9AF',
+                                                textDecoration: "line-through",
+                                                fontSize: 22
+                                            }}> {currentPrice} <span
+                                                style={{ fontSize: 18 }}>грн</span></p>
+    
+                                        </div>
+    
+                                        : <p style={{ margin: 0, position: 'absolute', bottom: 3 }}> {currentPrice} <span
+                                            style={{ fontSize: 18 }}>грн</span></p>}
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                </Card >
+    
+            )
 
-                <Card style={useStyles.root}>
-
-
-                    {newProduct ? <p style={useStyles.new}> Новинка</p> : ''}
-                    <CardActionArea style={{ height: '100%' }}>
-                        <div style={{ height: 200, width: 200, margin: ' 0 auto' }} >
-                            <CardMedia
-                                component="img"
-                                alt="Contemplative Reptile"
-                                
-                                image={imageUrls[0]}
-                                title="Contemplative Reptile"
-                                style={{ height: 200, width: 200, objectFit: 'contain' }}
-                            />
-                        </div>
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="h5" style={useStyles.nameProduct}>
-                                {name}
-                            </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p" style={useStyles.price}>
-                                {discount ?
-                                    <div style={{ bottom: 3, position: 'absolute', display: "flex", alignItems: "center" }}>
-                                        <p style={{ margin: 0 }}> {newPrice} <span
-                                            style={{ fontSize: 18 }}>грн</span></p>
-                                        <p style={{
-                                            margin: '0 0 0 13px',
-                                            color: '#A0A9AF',
-                                            textDecoration: "line-through",
-                                            fontSize: 22
-                                        }}> {currentPrice} <span
-                                            style={{ fontSize: 18 }}>грн</span></p>
-
-                                    </div>
-
-                                    : <p style={{ margin: 0, position: 'absolute', bottom: 3 }}> {currentPrice} <span
-                                        style={{ fontSize: 18 }}>грн</span></p>}
-                            </Typography>
-                        </CardContent>
-                    </CardActionArea>
-                </Card>
-            </Card >
-
-        )
+        }
     }, [products]) : '')
     return (
         <div style={{ margin: '50px 4px 80px' }}>  {/* overflow: "hidden", , height: 400  */}
