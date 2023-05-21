dd5795dbe47deea6d1247239bc9c409a63f4d177
Ihor <rybak.ihor.91@gmail.com>
2023-05-21 09:55:49 +0300

    userRequest_2 (test)

--- Client/src/Components/Layout/layout.component.jsx
+++ Client/src/Components/Layout/layout.component.jsx
@@ -4,6 +4,7 @@ import Header from '../Header/header.component.jsx';
 import Footer from '../Footer/footer.js';
 import { useDispatch } from "react-redux";
 import { loadProductsAsync } from '../../Redux/reducer.js';
+import UserRequest from '../UserRequest/UserRequest.jsx';
 function Layout(props) {
     const dispatch = useDispatch()
     useEffect(() => {
@@ -13,6 +14,8 @@ function Layout(props) {
     return (
         <>
             <Header />
+            {/* Test? */}
+            <UserRequest />
             <Outlet />
             <Footer />
         </>
