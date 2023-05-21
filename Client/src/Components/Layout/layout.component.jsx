efc35f429aec2cf0142089999bc41dfa49bbf7d5
Ihor <rybak.ihor.91@gmail.com>
2023-05-21 09:37:05 +0300

    userRequest_1 (test)

--- Client/src/Components/Layout/layout.component.jsx
+++ Client/src/Components/Layout/layout.component.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import {Outlet} from "react-router-dom";
 import Header from '../Header/header.component.jsx';
 import Footer from '../Footer/footer.js';
+import UserRequest from '../UserRequest/UserRequest.jsx';
 
 function Layout(props) {
     return (
         <>
             <Header/>
+            <UserRequest />
             <Outlet/>
             <Footer/>
         </>
